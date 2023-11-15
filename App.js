c90a04190d0a119070483d4eb36d56b03da1078f
ashishkumar52 <ashishkumar52@kpmg.com>
2023-11-15 12:27:21 +0530

    Added initial changes

---
+++ App.js
@@ -0,0 +1,39 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import InstructionScreen from './screens/InstructionScreen';
+
+
+const Stack = createNativeStackNavigator();
+
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Instruction'>
+        <Stack.Screen
+          options={{
+            headerTitle: () => (
+              <Text>
+                NMS
+              </Text>
+            ),
+            headerRight: () => (
+              <Button
+                onPress={() => alert('This is a button!')}
+                title="Setting"
+                color="#00cc00"
+              />
+            ),
+          }} name="Home" component={HomeScreen} />
+        <Stack.Screen name="Instruction" component={InstructionScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ screens/InstructionScreen.js
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import { Button, View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 0,
+        padding: 20,
+    },
+    titleText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#fff'
+    },
+    Instra: {
+        backgroundColor: '#666',
+        textAlign: 'center',
+        padding: 10,
+    },
+    textDesg: {
+        padding: 5,
+        paddingTop: 25
+        
+    }
+});
+
+
+
+const InstructionScreen = () => {
+    const navigation = useNavigation();
+    return (
+        <View
+            style={[
+                styles.container,
+                {
+                    flexDirection: 'column',
+                },
+            ]}>
+            <View style={styles.Instra}>
+                <Text style={styles.titleText}>INSTRUCTIONS</Text>
+            </View>
+
+            <View style={{ flex: 0, alignItems: 'left', }}>
+                <View style={styles.textDesg}>
+                    <Text >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</Text>
+                </View>
+
+                <View style={styles.textDesg}>
+                    <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</Text>
+                </View>
+
+                <View style={styles.textDesg}>
+                    <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</Text>
+                </View>
+
+                <View style={styles.textDesg}>
+                    <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</Text>
+                </View>
+
+                <View style={styles.textDesg}>
+                    <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</Text>
+                </View>
+
+                <View style={styles.textDesg}>
+                    <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</Text>
+                </View>
+            </View>  
+            <View style={{ color: '#fff', marginTop: 30}}>
+                    <Button
+                        title="I Agree"
+                        onPress={() => navigation.navigate('Home')}
+                    />
+            </View> 
+        </View>
+    );
+}
+
+export default InstructionScreen;
