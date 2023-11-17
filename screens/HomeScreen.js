eb94ae52fe4a69db4b3a725017800b1a1a78ae08
Puneeth-kpmg <puneethv>
2023-11-17 11:30:52 +0530

    pushing home screen code

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,44 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { Button } from 'react-native-elements';
+import { NavigationContainer,useNavigation} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { Keys, homeButton } from '../config/en'
 
-const HomeScreen = ()=> {
+import StyleSheets from '../css/CommonCss'
+const HomeScreen = () => {
+  const navigation = useNavigation();
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
+    <>
+      {/* <View style={StyleSheets.container}> */}
+      <View style={StyleSheets.online}>
+        <Text style={{ color: '#91c2ca', fontSize: 20 }}>{Keys.ONLINE} <Icon name='circle' size={20} style={{ margin: 10, }} color='#3abf33' /></Text>
+      </View>
+      <View style={StyleSheets.banner}>
+        <Text style={{ color: '#ffff', fontWeight: '700', fontSize: 20 }}>{Keys.HOMEPAGE_BANNER}</Text>
+      </View>
+      <View style={StyleSheets.buttonView}>
+        {
+          homeButton.map((button, index) => {
+            return (
+              <Button key={index} icon={
+                <Icon name={button.iconName} size={40} style={{ margin: 10, }} color='white' />
+              } title={button.buttonName} buttonStyle={StyleSheets.button} onPress={() => navigation.navigate(button.navigatonTo)}>
+              </Button>
+            )
+          })
+        }
+        <View style={{ paddingLeft: 60, margin: 5 }}>
+          <Text style={{ color: '#91c2ca', fontSize: 18, fontWeight: 500 }}>{Keys.HOMEPAGE_FOOTER_BANNER}</Text>
+        </View>
+      </View>
+      <View style={{ position: 'absolute', left: 0, right: 0, bottom: 15 }}>
+        <Button icon={<FontAwesome name="volume-control-phone" size={40} style={{ margin: 10, }} color='white' />
+        } title={Keys.CONTACT_NAME} buttonStyle={[StyleSheets.button, StyleSheets.footerButton]} onPress={() => { }}>
+        </Button></View>
+    </>
   );
 }
 
---
+++ screens/SymptomAssessMent.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View,Text } from 'react-native';
+import StyleSheets from '../css/CommonCss'
+
+const SymptomAssessMent = ()=> {
+ return (
+    <View style= {StyleSheets.container}>
+        <Text>IN PROGRESS</Text>
+    </View>
+ )
+}
+export default SymptomAssessMent;
